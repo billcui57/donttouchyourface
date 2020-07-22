1c7120e3d94329f8640700ae6d324d02e1b73897
Bill Cui <billcui2001@hotmail.com>
2020-07-22 10:24:53 -0400

    Facial recognition works with canvas

--- facial-recognition/src/App.js
+++ facial-recognition/src/App.js
@@ -1,57 +1,79 @@
 import React from 'react';
 
 import * as faceapi from 'face-api.js';
+import './App.css';
 class App extends React.Component {
 
 
 
-
+  //determines whether or not the user's browser supports a webcam or if they even have a webcam
   hasGetUserMedia() {
     return !!(navigator.mediaDevices &&
       navigator.mediaDevices.getUserMedia)
   }
 
+  //first thing to get run after the constructor and after the component is loaded
   async componentDidMount() {
 
+
+    //if user has a webcam
     if (this.hasGetUserMedia()) {
 
-
+      //gets the video element
       const video = document.getElementById('video')
 
+      //asyncronojsly loads all the faceapi models
       Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri('/models/'),
         faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
         faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
         faceapi.nets.faceExpressionNet.loadFromUri('/models')
-      ]).then(startVideo)
+      ]).then(startVideo) //and then starts the webcam footage
 
       function startVideo() {
         navigator.getUserMedia(
           { video: {} },
-          stream => video.srcObject = stream,
+          stream => video.srcObject = stream, //makes the video output what the webcam sees
           err => console.error(err)
         )
       }
 
+      //once the video starts playing...
       video.addEventListener('play', () => {
-        const canvas = this.refs.resultCanvas
+        //creates a canvas over the video element to draw on
+        const canvas = faceapi.createCanvasFromMedia(video)
+        document.getElementById("result").append(canvas)
+
+        //makes sure the dimensions of the canvas is the same as the video
         const displaySize = { width: video.width, height: video.height }
         faceapi.matchDimensions(canvas, displaySize)
+
+        //every 100ms...
         setInterval(async () => {
+
+          //detects the facelandmarks and expressions
           const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+
+
+          //resizes the detections coords to match the canvas display size
           const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+          //clears the canvas to draw new detection
           canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
 
+          //draws
           faceapi.draw.drawDetections(canvas, resizedDetections)
           faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
           faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+          //TODO: export rectangle
+
+        
         }, 100)
       })
     } else {
-      alert('getUserMedia() is not supported by your browser');
+      alert('Do you have a webcam?');
     }
-
-
   }
 
 
@@ -60,17 +82,9 @@ class App extends React.Component {
 
   render() {
     return (
-      
-      <div>
-        <canvas ref="resultCanvas"></canvas>
-        <video width="720" height="560" ref="videoFeed" muted autoPlay id="video"></video>
-        
-
+      <div id = "result">
+        <video width="720" height="560"  muted autoPlay id="video"></video>
       </div>
-
-
-
-
     );
   }
 
