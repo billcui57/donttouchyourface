3338969d953c4e09afbae47d38f382e737a18a54
Sean Zhang <seanzhang@seanslaptop.lan>
2020-08-12 20:21:07 -0400

    remove backend

--- backend/app/models/ai-shit/touch_face_detection.py
+++
@@ -1,5 +0,0 @@
-import facial_detection_pipe
-import hand_detection
-
-
-
--- backend/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export FLASK_APP=app
-export FLASK_ENV=development
-source ./venv/bin/activate
-python3 run.py &> console.log.txt & tail -f console.log.txt
-deactivate
