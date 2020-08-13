1b1252f3b4054bad2cdb98216e29b6be2576d889
Sean Zhang <seanzhang@seanslaptop.lan>
2020-08-12 20:17:51 -0400

    clean

---
+++ archive/README.md
@@ -0,0 +1,11 @@
+backend boi
+
+
+    Set execute permission on your script:
+    chmod +x script-name-here.sh
+    To run your script, enter:
+    ./script-name-here.sh
+    Another option is as follows to execute shell script:
+    sh script-name-here.sh
+    OR
+    bash script-name-here.sh
---
+++ archive/app/models/ai-shit/facial_detection_pipe.py
@@ -0,0 +1,56 @@
+from cv2 import cv2
+
+
+# Load the cascade (model)
+face_cascade = cv2.CascadeClassifier('./train/haarcascade_frontalface_default.xml')
+
+# To capture video from webcam.
+cap = cv2.VideoCapture(0)
+# To use a video file as input
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    #flip the frame
+    img = cv2.flip(img, 1)
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle that is the "hand"
+    handX, handY, handWidth, handHeight = 100, 100, 50, 50
+
+    cv2.rectangle(img, (handX, handY) , (handX+handWidth, handY+handHeight), 5, 2)
+
+    faceDetections = []
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        faceDetections.append({
+            "faceX": x,
+            "faceY": y,
+            "faceWidth": w,
+            "faceHeight": h
+        })
+        cv2.rectangle(img,(x,y), (x+w,y+h),1 , 2) #draw rect with the coordinates
+
+    for face in faceDetections:
+        if ((face['faceX'] < handX + handWidth) and
+            (face['faceX'] + face['faceWidth']  > handX) and
+            (face['faceY'] < handY + handHeight) and
+            (face['faceY'] + face['faceHeight']  > handY)):
+                print("intersecting")
+        else:
+            print("not intersecting")
+
+
+    # Display img on canvas (screen)
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+# Release the VideoCapture object
+cap.release()
